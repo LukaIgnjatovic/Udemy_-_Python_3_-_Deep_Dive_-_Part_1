5d66d91e5d23f45d89fdc0195c6630cdf83fde6d
Luka Ignjatović <ignjatovicluka@gmail.com>
2018-11-26 16:37:58 +0100

    Lesson 3 - Functions.

--- 03-functions.py
+++ 03-functions.py
@@ -22,6 +22,8 @@ def func_1():
 
 
 func_1()
+
+
 # Note that to "call" or "invoke" a function we need to use the "()". Calling the function without "()" will only give its memory location.
 
 
@@ -38,5 +40,37 @@ print(func_2("a", 3))
 # Even lists can be passed.
 print(func_2([1, 2], 3))
 
-# However, strings are not supported by the "*" operator.
-print(func_2("a", "b"))
+# However, strings are not supported by the "*" operator. The next line is commented because it would throw an error otherwise.
+# print(func_2("a", "b"))
+
+# Calling the function without the arguments will just print its definition and memory location.
+print(func_2)
+
+
+# Functions can call other functions in their execution.
+def func_3():
+    return func_4()
+
+
+def func_4():
+    return print("Running func_4")
+
+
+func_3()
+
+
+# Still, the proper order of execution is essential. The next lines are commented because they would throw an error otherwise.
+# def func_5():
+#     return func_6()
+
+
+# func_5()
+
+
+# def func_6():
+#     return print("Running func_5")
+
+# Lambda functions are also present in Python.
+fn1 = lambda x: x**2
+
+print(fn1(2))
