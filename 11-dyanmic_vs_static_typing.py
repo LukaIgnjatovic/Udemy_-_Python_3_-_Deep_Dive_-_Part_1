a3c915731c2ce9499865ed9023d381867d9c4879
Luka Ignjatović <ignjatovicluka@gmail.com>
2018-12-07 17:20:17 +0100

    Lesson 11 - Dynamic vs Static Typing.

--- 11-dyanmic_vs_static_typing.py
+++ 11-dyanmic_vs_static_typing.py
@@ -1,4 +1,4 @@
-# Python is dynamically typed language. This means that a variable does not have information about its type when defined - its type can change
+# Python is dynamically typed language. This means that variable does not have any information about its type when it is defined - its type can change
 # throughout code. When "type" function is called, it only returns the type of object that the variable is currently pointing at.
 a = "Hello"
 
