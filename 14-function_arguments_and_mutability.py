f9246073899dcc836a0a6a780cf2c77222c8da29
Luka Ignjatović <ignjatovicluka@gmail.com>
2018-12-11 13:42:51 +0100

    Lesson 14 - Function Arguments and Mutability.

--- 14-function_arguments_and_mutability.py
+++ 14-function_arguments_and_mutability.py
@@ -1,13 +1,13 @@
 # "modify_string" function is going to show how strings are immutable by printing the memory address before and after string concatenation.
 def modify_string(s):
-    print("Initial \"s\" memory address is {0}.".format(id(s)))
+    print("Initial \"s\" memory address is {0}.".format(hex(id(s))))
     s = s + " world."
-    print("Final \"s\" memory address is {0}.".format(id(s)))
+    print("Final \"s\" memory address is {0}.".format(hex(id(s))))
 
 
 my_string = "Hello"
 
-print("\"my_string\" memory address is {0}.".format(id(my_string)))
+print("\"my_string\" memory address is {0}.".format(hex(id(my_string))))
 
 modify_string(my_string)
 # By calling "modify_string" on "my_string" shows that "my_string" and initial "s" have the same address.
@@ -21,9 +21,9 @@ print(my_string)
 
 # "modify_list" function is going to show how lists are mutable by printing the memory address before and after appending elements to a list.
 def modify_list(lst):
-    print("Initial \"lst\" memory address is {0}.".format(id(lst)))
+    print("Initial \"lst\" memory address is {0}.".format(hex(id(lst))))
     lst.append(100)
-    print("Final \"lst\" memory address is {0}.".format(id(lst)))
+    print("Final \"lst\" memory address is {0}.".format(hex(id(lst))))
 
 
 my_list = [1, 2, 3]
@@ -41,9 +41,9 @@ print(my_list)
 
 # Immutable objects are generally safer when operating with them. However, this is not always true.
 def modify_tuple(t):
-    print("Initial \"t\" memory address is {0}.".format(id(t)))
+    print("Initial \"t\" memory address is {0}.".format(hex(id(t))))
     t[0].append(100)
-    print("Final \"t\" memory address is {0}.".format(id(t)))
+    print("Final \"t\" memory address is {0}.".format(hex(id(t))))
 
 
 my_tuple = ([1, 2], "a")
